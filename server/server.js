3fa957faecc1f3c83bb8173150d5823252f48b2b
Dev-AKL <lalabhinav19@gmail.com>
2025-09-28 20:35:08 +0530

    Added the backend logic of login and signup form

---
+++ server/server.js
@@ -0,0 +1,34 @@
+// server.js
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+//Routing for user authentication
+
+const authRoutes = require('./src/routes/authRoutes');
+
+app.use('/api/auth', authRoutes);
+
+// MongoDB Connection
+const MONGO_URI = process.env.MONGO_URI;
+mongoose.connect(MONGO_URI)
+  .then(() => console.log('MongoDB connected successfully'))
+  .catch(err => console.error('MongoDB connection error:', err));
+
+// Basic route for testing
+app.get('/', (req, res) => {
+  res.send('Welcome to the College ERP API!');
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
